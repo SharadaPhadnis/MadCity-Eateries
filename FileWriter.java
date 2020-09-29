e596fbff4a57faede317898fb32b7095de3b6f2f
kdev792 <devarakonda2@wisc.edu>
2020-09-29 14:21:03 -0500

    Add files via upload

--- FileWriter.java
+++ FileWriter.java
@@ -7,14 +7,13 @@
 // Lecturer: Florian Heimerl
 // Notes to Grader: Utilized StackOverflow, Greeks for Geeks, and other CS websites/forums for help. Did not copy code but used it as a template/skeleton. Also collaborated a lot with Yi, fellow data wrangler. Bounced ideas off each other and made similar working code that could fit easily into our other group members' projects. 
 // Notes to Grader (cont.): I used this following link to help me with paths and finding directories on my machine in eclipse: https://stackoverflow.com/questions/4871051/how-to-get-the-current-working-directory-in-java
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class FileWriter {
 	
@@ -27,28 +26,23 @@ public class FileWriter {
 		 String currentDirectory = System.getProperty("user.dir");
 		 
 		 //That location and the location of the csv file we need to use are stored in a Path type variable
-		 Path filePath = Paths.get(currentDirectory, "restaurants.csv");
+		 String filePath = (Paths.get(currentDirectory, "restaurants.csv")).toString();
 		 
 		 //We create a mock ArrayList variable of type String[] and use the method created below to iterate through the csv file after we have found its location in our machine using the path variables
-		 ArrayList<String[]> iterator = FileWriter.restaurantReader(filePath.toString());
+		 ArrayList<String[]> iterator = FileWriter.restaurantReader(filePath);
 		 
 		 //then a for loop loops through the ArrayList of type String[] and then properly formats the name and number of the restaurant to be ready for the backend usage and then adds it to the HashTableMap object which can then be used by the other roles in the project
 		 for(int i = 0; i < iterator.size(); i++) {
 			 
 			 //because both the names and numbers had quotes around them that we no longer wanted
-			 String restaurantName = iterator.get(i)[0].replace('"', ' ');
+			 iterator.get(i)[0] = iterator.get(i)[0].substring(1, (iterator.get(i)[0].length() - 1));
 			 
-			 //gets rid of unnecessary spaces
-			 restaurantName = restaurantName.trim();
+			 String restaurantName = iterator.get(i)[0];
 			 
 			 //because both the names and numbers had quotes around them that we no longer wanted
-			 String restaurantNumber = iterator.get(i)[1].replace('"', ' ');
+			 iterator.get(i)[1] = iterator.get(i)[0].substring(1, (iterator.get(i)[1].length() - 1));
 			 
-			//gets rid of unnecessary spaces
-			 restaurantNumber = restaurantNumber.trim();
-			 
-			 //because the numbers were too big to store in a variable so we got rid of the area code
-			 restaurantNumber = restaurantNumber.substring(3);
+			 String restaurantNumber = iterator.get(i)[1];
 			 
 			 //used our generic put method to add it to the hashMap
 			 hashMap.put(restaurantNumber, restaurantName);
@@ -82,14 +76,14 @@ public class FileWriter {
 		//FileReader type variable that holds the file with the csv info in it
 		FileReader fileReader = new FileReader(restaurants);
 					
-		//BufferedReader used to read the FileReader type variable
-		BufferedReader bufferedReader = new BufferedReader(fileReader);
+		//Scanner used to read the FileReader type variable
+		Scanner scanner = new Scanner(fileReader);
 		
 		//While loop to iterate as long as the line to read is not null meaning there are still restaurants to look at in the csv file
-		while(bufferedReader.readLine()!= null) {
+		while(scanner.nextLine()!= null) {
 			
-			//the String type variable reader now holds the line given/read by the bufferedReader
-			reader = bufferedReader.readLine();
+			//the String type variable reader now holds the line given/read by the scanner
+			reader = scanner.nextLine();
 			
 			//the String[] variable now holds both name and number based on which side of the comma they are on
 			restaurantInfo = reader.split(",");
@@ -99,19 +93,14 @@ public class FileWriter {
 				
 		}
 		
-		//the bufferedReader is closed
-		bufferedReader.close();
+		//the scanner is closed
+		scanner.close();
 		
 		}
 		
 		//if there are any FileNotFoundExceptions -- could only really come from the File restaurants variable -- they will be caught here and a message will print
 		catch(FileNotFoundException e) {
 			System.out.print("File not found");
-		}
-		
-		//If there are any IOExceptions they will be caught here and a message will be printed
-		catch(IOException e) {
-			System.out.print("Input or Outpout did not work");
 		}
 		
 		//The ArrayList of type String[] will be returned here
