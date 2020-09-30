0a3844f0b43d845db059d5bcd4f85fa14e59873f
YIXIAO0 <54782454+YIXIAO0@users.noreply.github.com>
2020-09-29 20:45:19 -0500

    Update RestaurantHashTableMap.java

--- RestaurantHashTableMap.java
+++ RestaurantHashTableMap.java
@@ -11,7 +11,7 @@ import java.util.NoSuchElementException;
 public class RestaurantHashTableMap<K, V> extends HashTableMap {
 	private int capacity;
 	private int size;
-	private LinkedList[] list;
+	private LinkedList<RestaurantPair>[] list;
 	
 	//default constructor
 	public RestaurantHashTableMap() {
